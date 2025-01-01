c97ba719a0f10f5c916ba77af89b6c9d4a39d115
Ashique <ashique9496@gmail.com>
2025-01-01 17:25:28 +0530

    service section partially done

---
+++ Kalabam/src/components/Service/Service.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import "./Service.scss";
+
+const Service = () => {
+    const serviceItems = [
+        {
+            color: "#0d547d",
+            title: "Pooja",
+            icon: "bx bx-spa",
+            desc: "Experience divine blessings through our meticulously performed poojas and rituals.",
+        },
+        {
+            color: "#6c2281",
+            title: "Astrology",
+            icon: "bx bx-moon",
+            desc: "Seek guidance from our experienced astrologers for life’s important decisions.",
+        },
+        {
+            color: "green",
+            title: "Festival",
+            icon: "bx bx-party",
+            desc: "Join us in celebrating vibrant temple festivals that bring the community together.",
+        },
+        {
+            color: "#a5241b",
+            title: "Prasadam",
+            icon: "bx bx-food-menu",
+            desc: "Relish the divine prasadam prepared with devotion and care.",
+        },
+    ];
+    return (
+        <div id="service" className="service">
+            <div className="shape-2"></div>
+            <div className="container">
+                <div className="row service_row">
+                    <div className="service_title">
+                        <h3>
+                            <i class="bx bx-cog"></i>Services
+                        </h3>
+                        <h4>
+                            Experience the Divine <span>Through Our Services</span>
+                        </h4>
+                    </div>
+                    <div className="row service_items">
+                        {serviceItems.map((item, index) => (
+                            <div  key={index} className="col-xl-3 col-lg-3 col-md-12 item1">
+                                <div className="card">
+                                    <div className="icon" style={{ backgroundColor: item.color }}>
+                                        <i class={item.icon}></i>
+                                    </div>
+                                    <div className="content">
+                                        <h5>{item.title}</h5>
+                                        <p>{item.desc}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Service;
--- Kalabam/src/pages/Home.jsx
+++ Kalabam/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "../components/Hero/Hero";
 import NavBar from "../components/NavBar/NavBar";
 import About from "../components/About/About";
+import Service from "../components/Service/Service";
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
             {/* <NavBar /> */}
             <Hero />
             <About />
+            <Service />
         </>
     );
 };
